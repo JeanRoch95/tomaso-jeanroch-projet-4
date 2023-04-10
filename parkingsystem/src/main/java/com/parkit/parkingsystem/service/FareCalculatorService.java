7a5b3bb2a318210272ce636cc4bd2dc5f33a60fd
Jean-Roch Tomaso <9r7r96zfc397-student@anonymous-codio.com>
2023-04-10 19:30:57 +0000

    5% feature, correcting after mentoring

--- parkingsystem/src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ parkingsystem/src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -7,6 +7,10 @@ import com.parkit.parkingsystem.model.Ticket;
 public class FareCalculatorService {
 
     public void calculateFare(Ticket ticket){
+        this.calculateFare(ticket, false);
+    }
+
+    public void calculateFare(Ticket ticket, boolean discount){
         if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
@@ -17,24 +21,24 @@ public class FareCalculatorService {
         //TODO: Some tests are failing here. Need to check if this logic is correct
         long duration = ((outHour - inHour) / 1000 / 60);
 
+        if(duration < 30){
+            ticket.setPrice(duration * Fare.VEHICULE_RATE_LESS_THAN_30_MIN);
+            return;
+        }
+
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
-                if(duration < 30){
-                    ticket.setPrice(duration * (Fare.VEHICULE_RATE_LESS_THAN_30_MIN));
-                }else{
                     ticket.setPrice(duration * (Fare.CAR_RATE_PER_HOUR) / 60);
-                }
                 break;
             }
             case BIKE: {
-                if(duration < 30){
-                    ticket.setPrice(duration * (Fare.VEHICULE_RATE_LESS_THAN_30_MIN));
-                }else{
                     ticket.setPrice(duration * (Fare.BIKE_RATE_PER_HOUR) / 60);
-                }
                 break;
             }
             default: throw new IllegalArgumentException("Unkown Parking Type");
+        }
+        if(discount){
+            ticket.setPrice(ticket.getPrice() * Fare.DISCOUNT_RATE);
         }
     }
 }
