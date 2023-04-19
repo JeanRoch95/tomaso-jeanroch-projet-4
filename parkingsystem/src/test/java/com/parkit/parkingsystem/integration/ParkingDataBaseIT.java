1c5349f87d2aa144a5bcceef649f86cd9aba2b27
Jean-Roch Tomaso <9r7r96zfc397-student@anonymous-codio.com>
2023-04-19 16:40:03 +0000

    Adding integration test ParkingService

--- parkingsystem/src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ parkingsystem/src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -2,8 +2,15 @@ package com.parkit.parkingsystem.integration;
 
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
+
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.Ticket;
+import com.parkit.parkingsystem.model.ParkingSpot;
+
+import com.parkit.parkingsystem.constants.Fare;
+import com.parkit.parkingsystem.constants.ParkingType;
+
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
 import org.junit.jupiter.api.AfterAll;
@@ -14,6 +21,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
@@ -48,18 +60,62 @@ public class ParkingDataBaseIT {
     }
 
     @Test
-    public void testParkingACar(){
+    public void testParkingACar()throws Exception {
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+
         parkingService.processIncomingVehicle();
+        Ticket ticket = ticketDAO.getTicket(inputReaderUtil.readVehicleRegistrationNumber());
+
+        assertEquals(ticket.getId(), 1);
+        assertFalse(ticket.getParkingSpot().isAvailable());
+        assertEquals(parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR), 2);
+
         //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
     }
 
     @Test
-    public void testParkingLotExit(){
-        testParkingACar();
+    public void testParkingLotExit()throws Exception {
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        ParkingSpot parkingSpot = parkingService.getNextParkingNumberIfAvailable();
+        Date inTime = new Date(System.currentTimeMillis() - (60 * 60 * 1002));
+        Ticket ticket = new Ticket();
+        ticket.setParkingSpot(parkingSpot);
+        ticket.setVehicleRegNumber(inputReaderUtil.readVehicleRegistrationNumber());
+        ticket.setPrice(0);
+        ticket.setInTime(inTime);
+        ticket.setOutTime(null);
+        ticketDAO.saveTicket(ticket);
+
         parkingService.processExitingVehicle();
+        ticket = ticketDAO.getTicket("ABCDEF");
+
+        assertEquals(1, ticketDAO.getNbTicket(inputReaderUtil.readVehicleRegistrationNumber()));
+        assertNotNull(ticket.getOutTime());
+        assertEquals(Fare.CAR_RATE_PER_HOUR, ticket.getPrice());
         //TODO: check that the fare generated and out time are populated correctly in the database
+    }
+
+    @Test
+    public void testParkingLotExitRecurringUser()throws Exception{
+        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        parkingService.processIncomingVehicle();
+
+        ParkingSpot parkingSpot = parkingService.getNextParkingNumberIfAvailable();
+        Date inTime = new Date(System.currentTimeMillis() - (60 * 60 * 1000));
+        Ticket ticket = new Ticket();
+        ticket.setParkingSpot(parkingSpot);
+        ticket.setVehicleRegNumber(inputReaderUtil.readVehicleRegistrationNumber());
+        ticket.setPrice(0);
+        ticket.setInTime(inTime);
+        ticket.setOutTime(null);
+        ticketDAO.saveTicket(ticket);
+
+        parkingService.processExitingVehicle();
+        ticket = ticketDAO.getTicket("ABCDEF");
+
+        assertEquals(2, ticketDAO.getNbTicket(inputReaderUtil.readVehicleRegistrationNumber()));
+        assertEquals(Fare.CAR_RATE_PER_HOUR * Fare.DISCOUNT_RATE, ticket.getPrice());
+
     }
 
 }
